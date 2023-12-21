e84f6f77356cde37174949642fcb03a46a3af0ed
Gregor Karetka <gkaretka@cisco.com>
2023-12-21 12:03:14 +0100

    Bugfix #5 - string in where not working
    - String in WHERE statement not working
    - Update tests

--- src/duckberg/sqlparser.py
+++ src/duckberg/sqlparser.py
@@ -46,4 +46,5 @@ class DuckBergSQLParser:
 
     def extract_where_conditions(self, where_statement: list[sqlparse.sql.Where]):
         comparison = sqlparse.sql.TokenList(where_statement[1:])
-        return parser.parse(str(comparison))
+        where_condition = str(comparison).replace('"', "'")  # revert from double to single
+        return parser.parse(where_condition)
